a26c802b23f9566e10ecc8423cd1de1dd3eb370f
Md Sazzad Hossain Salim <34300692+sazzad362@users.noreply.github.com>
2023-06-01 02:19:48 +0600

    update day 2

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,6 +10,23 @@
 			$('footer ul').removeClass('menu-active');
 			const id = $(this).attr('data-item');
 			$('#'+id).addClass('menu-active');
-		})
+		});
+
+		// One page scrooling 
+		// $('.scrollTo').click(function() {
+		//   if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+		//     var target = $(this.hash);
+		//     var headerH='0';
+		//     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+		//     if (target.length) {
+		//       $('html, body').animate({
+		//         scrollTop: target.offset().top-headerH+"px"
+		//       }, 1000);
+		//       return false;
+		//     }
+		//   }
+		// });
+
+
 	});// DOM Ready
 }(jQuery)); // IIFE
