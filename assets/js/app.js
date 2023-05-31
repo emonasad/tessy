99cc48f1150f48c88016cc4d3c640cab6341c1ec
Md Sazzad Hossain Salim <34300692+sazzad362@users.noreply.github.com>
2023-05-31 07:38:30 +0600

    Just init

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+// IIFE with jQuery Wrapper
+(function ($) {
+	'use strict';
+	/*----------------------------------*/
+	/* Document Ready
+	/*----------------------------------*/
+	$(document).ready(function () {
+		var clicks = 0;
+		$('footer .title').on('click', function(){
+			$('footer ul').removeClass('menu-active');
+			const id = $(this).attr('data-item');
+			$('#'+id).addClass('menu-active');
+		})
+	});// DOM Ready
+}(jQuery)); // IIFE
