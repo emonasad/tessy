9862f725662c34e6427356f604d5f9982fb55db5
Md Sazzad Hossain Salim <34300692+sazzad362@users.noreply.github.com>
2023-06-02 00:20:56 +0600

    translate text updated

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,27 +6,34 @@
 	/*----------------------------------*/
 	$(document).ready(function () {
 		var clicks = 0;
-		$('footer .title').on('click', function(){
+		$('footer .title').on('click', function () {
 			$('footer ul').removeClass('menu-active');
 			const id = $(this).attr('data-item');
-			$('#'+id).addClass('menu-active');
+			$('#' + id).addClass('menu-active');
 		});
 
 		// One page scrooling 
-		// $('.scrollTo').click(function() {
-		//   if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-		//     var target = $(this.hash);
-		//     var headerH='0';
-		//     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-		//     if (target.length) {
-		//       $('html, body').animate({
-		//         scrollTop: target.offset().top-headerH+"px"
-		//       }, 1000);
-		//       return false;
-		//     }
-		//   }
-		// });
-
+		$('.scrollTo').click(function() {
+		  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+			closeNav();
+		    var target = $(this.hash);
+		    var headerH='104';
+		    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+		    if (target.length) {
+		      $('html, body').animate({
+		        scrollTop: target.offset().top-headerH+"px"
+		      }, 0);
+		      return false;
+		    }
+		  }
+		});
 
 	});// DOM Ready
 }(jQuery)); // IIFE
+
+function openNav() {
+	document.getElementById("mySidenav").style.right = "0px";
+}
+function closeNav() {
+	document.getElementById("mySidenav").style.right = "-300px";
+}
